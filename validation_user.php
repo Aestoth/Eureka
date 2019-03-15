1a38f6a455f11d8c765ba33bb0c2b954b6cc75c4
Antho <claude.anthony@bluewin.ch>
2019-03-15 17:41:17 +0100

    validation user

--- validation_user.php
+++ validation_user.php
@@ -1,25 +1,25 @@
 <?PHP
-    require_once 'connexion.php';
-    require_once 'bit_count.php';
-    $appliBD = new Connexion();
+require_once 'connexion.php';
+require_once 'bit_count.php';
+$appliBD = new Connexion();
 
 
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $password = $_POST['password'];
-    $description = $_POST['description'];
-    $email = $_POST['email'];
-    $telephone = $_POST['telephone'];
-    $avatar = $_POST['avatar'];
-    $jourDisponibles = to_bitfield($_POST['jourDisponibles']);
-    $derniereConnexion =  date('Y-m-d');
-    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$password = $_POST['password'];
+$description = $_POST['description'];
+$email = $_POST['email'];
+$telephone = $_POST['telephone'];
+$avatar = $_POST['avatar'];
+$jourDisponibles = to_bitfield($_POST['jourDisponibles']);
+$derniereConnexion =  date('Y-m-d');
+$passwordHash = password_hash($password, PASSWORD_BCRYPT);
 
-    $appliBD->insertEtudiant($nom, $prenom, $passwordHash, $description, $email, $telephone, $avatar, $jourDisponibles, $derniereConnexion);
+$appliBD->insertEtudiant($nom, $prenom, $passwordHash, $description, $email, $telephone, $avatar, $jourDisponibles, $derniereConnexion);
     
     
                    
      
-    print_r($_POST);
+print_r($_POST);
 
 ?>
