9e616d20764deebb302aa5e34f16af7827726b00
Gutembergue <gmascarenha3001@gmail.com>
2019-03-16 23:01:59 +0100

    conflit

--- validation_etudiant.php
+++ validation_etudiant.php
@@ -1,38 +1,27 @@
 <?PHP
-require_once 'connexion.php';
-require_once 'bit_count.php';
-$appliBD = new Connexion();
+    require_once 'connexion.php';
+    require_once 'bit_count.php';
+    $appliBD = new Connexion();
 
 
-$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$password = $_POST['password'];
-$description = $_POST['description'];
-$email = $_POST['email'];
-$telephone = $_POST['telephone'];
-$avatar = $_POST['avatar'];
-$jourDisponibles = to_bitfield($_POST['jourDisponibles']);
-$derniereConnexion =  date('Y-m-d');
-$passwordHash = password_hash($password, PASSWORD_BCRYPT);
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $password = $_POST['password'];
+    $description = $_POST['description'];
+    $email = $_POST['email'];
+    $telephone = $_POST['telephone'];
+    $avatar = $_POST['avatar'];
+    $jourDisponibles = to_bitfield($_POST['jourDisponibles']);
+    $derniereConnexion =  date('Y-m-d');
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
 
-<<<<<<< HEAD:validation_user.php
-$appliBD->insertEtudiant($nom, $prenom, $passwordHash, $description, $email, $telephone, $avatar, $jourDisponibles, $derniereConnexion);
-    
-    
-                   
-     
-print_r($_POST);
 
-?>
-=======
     $idEtudiant = $appliBD->insertEtudiant($nom, $prenom, $passwordHash, $description, $email, $telephone, $avatar, $jourDisponibles, $derniereConnexion);
 
 
     foreach ($_POST["Motsclesetudiant"] as $value) {
         $appliBD->insertMotCles_etudiant($idEtudiant, $value);
     }
-
-
 
         header("Location: page-profil-etudiant.php?id=$idEtudiant");
 
@@ -42,4 +31,3 @@ print_r($_POST);
 
 
 ?>
->>>>>>> master:validation_etudiant.php
